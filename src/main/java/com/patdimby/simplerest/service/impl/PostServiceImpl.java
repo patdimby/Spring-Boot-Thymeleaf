70dca76587844a689cf6ac99100080aaff3b5a43
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-07-28 06:16:03 +0300

    MyBatis
    Add MyBatis library.

--- src/main/java/com/patdimby/simplerest/service/impl/PostServiceImpl.java
+++ src/main/java/com/patdimby/simplerest/service/impl/PostServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @Service
 public class PostServiceImpl implements PostService {
 
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public PostServiceImpl(PostRepository postRepository) {
         this.postRepository = postRepository;
